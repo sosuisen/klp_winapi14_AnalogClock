8e4067509c257dc89e7f30345922c66a522d1db6
sosuisen <hidekazu.kubota@gmail.com>
2023-01-20 15:16:30 +0900

    create haihu ver

--- AnalogClock/resource.h
+++ AnalogClock/resource.h
@@ -6,10 +6,6 @@
 #define IDC_LIST1                       1001
 #define IDC_COMBO1                      1002
 #define IDC_COMBO2                      1003
-#define IDC_RADIO1                      1004
-#define IDC_RADIO_DIGITAL               1004
-#define IDC_RADIO2                      1005
-#define IDC_RADIO_ANALOG                1005
 
 // Next default values for new objects
 // 
@@ -17,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
